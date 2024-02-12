3598e3d02fabc9193560621985370090336ac59b
sovestfn <sovest.abbata@gmail.com>
2024-02-12 10:23:23 +0600

    p_14_4

---
+++ Practice_14_4/Practice_14_4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+void userString() {
+    cout << "Введите строку: ";
+    string myString = "";
+    getline(cin, myString);
+
+    if (!myString.empty()) {
+        int stringLength = myString.length();
+        char ch1 = myString[0];
+        char ch2 = myString[(myString.length()) - 1];
+
+        string firstChar(1, ch1);
+        string lastChar(1, ch2);
+
+        if (firstChar == " " || "   ") {
+            firstChar = "пробел/табуляция";
+        }
+
+        if (lastChar == " " || "   ") {
+            lastChar = "пробел/табуляция";
+        }
+
+        cout << "\n Длина строки: " << stringLength << "\n первый символ: " << firstChar << "\n последний символ: " << lastChar << "\n";
+        
+    } 
+    else {
+        cout << "\n Ничего не введено \n";
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    userString();
+
+    return 0;    
+}
+
